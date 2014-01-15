7ccbfebe9820fe0e3b50c09997fbe0ee33eea66e
Stanko Krtalic Rusendic <stanko.krtalic@gmail.com>
2014-01-15 15:28:11 +0100

    Initial Commit

---
+++ BCC4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BCC4::Application.config.secret_key_base = '784cae5c23134c5d3c889ab1e3b2b8bfa6b4b99478f137eb81709766f01cec3c7d368f96eade24f2d41e24b415aeec1e804d1473fa6085e0d3379a2dd47648e3'
